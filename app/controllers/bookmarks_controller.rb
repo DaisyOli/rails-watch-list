dc6697e79efdc29a928bfe523272b527c7843492
daisy oliani <daisy.oliani@gmail.com>
2024-11-14 16:55:11 +0100

    movie watch

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,20 +1,33 @@
 class BookmarksController < ApplicationController
-
   def new
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:list_id])  # Trouve la `list` parente
-    @bookmark = @list.bookmarks.new(bookmark_params)  # Crée un nouveau `bookmark` associé à la `list`
-  
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.new(bookmark_params)
+
     if @bookmark.save
       redirect_to list_path(@list), notice: "Bookmark created!"
     else
-      render :new # Affiche le formulaire `new` si sauvegarde échoue
+      flash.now[:alert] = "There was an error creating the bookmark."
+      render "lists/show"
+    end
+  end
+
+  def destroy
+    @list = List.find_by(id: params[:list_id]) # Utilise find_by pour éviter une exception si non trouvé
+    @bookmark = @list&.bookmarks.find(params[:id]) if @list # Récupère le bookmark si la liste est trouvée
+  
+    if @bookmark
+      @bookmark.destroy
+      redirect_to list_path(@list), status: :see_other
+    else
+      redirect_to lists_path, alert: "List or Bookmark not found."
     end
   end
   
+
   private
 
   def bookmark_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     resources :lists do
-      resources :bookmarks, only: [:new, :create]
+      resources :bookmarks, only: [:new, :create, :destroy]
     end
     get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
