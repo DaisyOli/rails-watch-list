642d9dec8fd0f8953fd2429d8c24d2a077538f9d
daisy oliani <daisy.oliani@gmail.com>
2024-11-15 16:36:35 +0100

    watch-list new

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,4 +4,5 @@ class Bookmark < ApplicationRecord
 
   validates :movie, uniqueness: { scope: :list, message: "has already been added to this list" }
   validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie_id, presence: true
 end
